e132c10917291fd2e22b213302b946f34505ae53
BPsoda <74891655+BPsoda@users.noreply.github.com>
2021-10-23 10:59:16 +0800

    Add: merge page1 and page2. Add keywords and authors formatting.

--- server.py
+++ server.py
@@ -19,6 +19,7 @@ except:
     print('Fail to connect to the database.')
     exit(-1)
 
+
 @app.route('/')
 def welcome():
     return flask.render_template('/index.html')
@@ -52,24 +53,40 @@ def SearchbyId():
     content = cursor.fetchall()
     return flask.render_template('/pages/page3.html', flag=True, labels=labels, content=content)
 
-@app.route('/pages/page1')
-def randomDisplay():
-    '''Random display 10 papers'''
-    # get table colomn name
-    cursor.execute("SELECT column_name FROM information_schema.columns \
-                    WHERE TABLE_SCHEMA='ieei_web' AND table_name='paper'")
-    labels = cursor.fetchall()
-    labels = [i[0] for i in labels]
-    # get 10 table content
-    cursor.execute('SELECT * FROM paper \
-                    ORDER BY RAND() \
-                    LIMIT 10')
-    content = cursor.fetchall()
-    return flask.render_template('/pages/page1.html', labels=labels, content=content)
-    # return flask.render_template('/pages/page1.html', content=content)
+# @app.route('/pages/page1')
+# def randomDisplay():
+#     '''Random display 10 papers'''
+#     # get table colomn name
+#     cursor.execute("SELECT column_name FROM information_schema.columns \
+#                     WHERE TABLE_SCHEMA='ieei_web' AND table_name='paper'")
+#     labels = cursor.fetchall()
+#     labels = [i[0] for i in labels]
+#     # get 10 table content
+#     cursor.execute('SELECT * FROM paper \
+#                     ORDER BY RAND() \
+#                     LIMIT 10')
+#     content = cursor.fetchall()
+#     return flask.render_template('/pages/page1.html', labels=labels, content=content)
+#     # return flask.render_template('/pages/page1.html', content=content)
 
-@app.route('/pages/page2')
-def randomDisplay2():
+# @app.route('/pages/page2')
+# def randomDisplay2():
+#     '''Random display 10 papers'''
+#     # get table colomn name
+#     cursor.execute("SELECT column_name FROM information_schema.columns \
+#                     WHERE TABLE_SCHEMA='ieei_web' AND table_name='paper'")
+#     labels = cursor.fetchall()
+#     labels = [i[0] for i in labels]
+#     # get 10 table content
+#     cursor.execute('SELECT * FROM paper \
+#                     ORDER BY RAND() \
+#                     LIMIT 10')
+#     content = cursor.fetchall()
+#     return flask.render_template('/pages/page2.html', labels=labels, content=content)
+#     # return flask.render_template('/pages/page1.html', content=content)
+
+@app.route('/pages/<page>')
+def randomDisplay(page):
     '''Random display 10 papers'''
     # get table colomn name
     cursor.execute("SELECT column_name FROM information_schema.columns \
@@ -81,8 +98,15 @@ def randomDisplay2():
                     ORDER BY RAND() \
                     LIMIT 10')
     content = cursor.fetchall()
-    return flask.render_template('/pages/page2.html', labels=labels, content=content)
-    # return flask.render_template('/pages/page1.html', content=content)
+    # format data
+    for i in content[3:5]:
+        i = i.strip('[]').split(',')
+        for j in i:
+            j = j.strip('\"')
+        i = '\n'.join(i)
+        
+    return flask.render_template('/pages/{}.html'.format(page), labels=labels, content=content)
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
