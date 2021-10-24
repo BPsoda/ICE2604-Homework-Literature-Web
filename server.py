9e2e923b391b07695b045f4a911228037e0cbc0b
BPsoda <74891655+BPsoda@users.noreply.github.com>
2021-10-24 17:12:24 +0800

    Remove unused code.

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from re import search
 import flask
 from flask.helpers import url_for
 import pymysql
@@ -30,20 +29,17 @@ def SearchbyId():
     '''Search paper by paper id'''
     # retrive paper id from the form 
     if flask.request.method == 'POST':
-        print('Using POST method')
         paperid = flask.request.form['searchField']
-        print(paperid)
     else:
-        print('Using GET method.')
         paperid = flask.request.args.get('searchField','')
-    print('Using GET method.')
-    print(paperid)
+
     # if user hasn't input paper id, return blank table
     if len(paperid) < 1 : 
         return flask.render_template('/pages/page3.html', flag=False, alert=False)
     # if the paperid is illegal, raise alert
     if (not paperid.isdigit()) or int(paperid) > 9999:
         return flask.render_template('/pages/page3.html', flag=False, alert = True)
+
     # get table colomn name
     cursor.execute("SELECT column_name FROM information_schema.columns \
                     WHERE TABLE_SCHEMA='ieei_web' AND table_name='paper'")
@@ -53,51 +49,18 @@ def SearchbyId():
     cursor.execute('SELECT * FROM paper \
                     WHERE paper_id={}'.format(paperid))
     content = cursor.fetchall()
-    print(content)
-    content = list(content)
+
     # format data
+    content = list(content)
     for h in range(len(content)):
         content[h] = list(content[h])
-        print(content[h])
         for i in range(3, 5):
             content[h][i] = content[h][i].strip('[]').split(', ')
             for j in range(len(content[h][i])):
                 content[h][i][j] = content[h][i][j].strip('"')
             content[h][i] = '\n'.join(content[h][i])
-            print(content[h][i])
     return flask.render_template('/pages/page3.html', flag=True, labels=labels, content=content, alert=False)
 
-# @app.route('/pages/page1')
-# def randomDisplay():
-#     '''Random display 10 papers'''
-#     # get table colomn name
-#     cursor.execute("SELECT column_name FROM information_schema.columns \
-#                     WHERE TABLE_SCHEMA='ieei_web' AND table_name='paper'")
-#     labels = cursor.fetchall()
-#     labels = [i[0] for i in labels]
-#     # get 10 table content
-#     cursor.execute('SELECT * FROM paper \
-#                     ORDER BY RAND() \
-#                     LIMIT 10')
-#     content = cursor.fetchall()
-#     return flask.render_template('/pages/page1.html', labels=labels, content=content)
-#     # return flask.render_template('/pages/page1.html', content=content)
-
-# @app.route('/pages/page2')
-# def randomDisplay2():
-#     '''Random display 10 papers'''
-#     # get table colomn name
-#     cursor.execute("SELECT column_name FROM information_schema.columns \
-#                     WHERE TABLE_SCHEMA='ieei_web' AND table_name='paper'")
-#     labels = cursor.fetchall()
-#     labels = [i[0] for i in labels]
-#     # get 10 table content
-#     cursor.execute('SELECT * FROM paper \
-#                     ORDER BY RAND() \
-#                     LIMIT 10')
-#     content = cursor.fetchall()
-#     return flask.render_template('/pages/page2.html', labels=labels, content=content)
-#     # return flask.render_template('/pages/page1.html', content=content)
 
 @app.route('/pages/<page>')
 def randomDisplay(page):
@@ -115,13 +78,11 @@ def randomDisplay(page):
     # format data
     for h in range(len(content)):
         content[h] = list(content[h])
-        print(content[h])
         for i in range(3, 5):
             content[h][i] = content[h][i].strip('[]').split(', ')
             for j in range(len(content[h][i])):
                 content[h][i][j] = content[h][i][j].strip('"')
             content[h][i] = '\n'.join(content[h][i])
-            print(content[h][i])
         
     return flask.render_template('/pages/{}.html'.format(page), labels=labels, content=content)
 
